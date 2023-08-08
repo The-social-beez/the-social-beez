8ef01a1948b97fa1e923b5d8c90054c00e039c9d
Aniket <57931002+aniket-dandekar@users.noreply.github.com>
2023-08-08 19:32:41 +0530

    mobile touch move

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,38 @@
 // import Link from 'next/link'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, RefObject } from 'react'
 import styles from './Header.module.css'
 
 type headerProps = {}
 
 export default function Header({ }: headerProps) {
 
-  const leftRef = useRef(null);
+  const containerRef: RefObject<HTMLDivElement> = useRef(null);
 
   const [leftWidth, setLeftWidth] = useState(50.45)
+  const [isMoving, setIsMoving] = useState(false)
 
   const handleMove = (e: any) => {
     setLeftWidth(e.clientX / window.innerWidth * 100);
+    // console.log(containerRef.current.parentElement)
+  }
+
+  const handleTouchMove = (e: any) => {
+    if (isMoving  &&  e.touches[0].clientX / window.innerWidth * 100 < 100) {
+        setLeftWidth(e.touches[0].clientX / window.innerWidth * 100);
+    }
   }
 
   useEffect(() => {
-    document.getElementById('header')!.addEventListener('mousemove', (e)=>{
+    document.getElementById('header')!.addEventListener('mousemove', (e) => {
       setLeftWidth(e.clientX / window.innerWidth * 100);
     })
   }, [])
-  
+
 
   return (
     <>
-      <div onMouseMove={handleMove} onTouchMove={handleMove} className="h-[80vh] md:h-[100vh]">
-        <div ref={leftRef} style={{ width: `${leftWidth}%` }} className={styles.side + " " + styles.left_side}>
+      <div ref={containerRef} onMouseMove={handleMove} onTouchStart={() => setIsMoving(true)} onTouchMove={handleTouchMove} onTouchEnd={() => setIsMoving(false)} className="h-[80vh] md:h-[100vh]">
+        <div style={{ width: `${leftWidth}%` }} className={styles.side + " " + styles.left_side}>
           <h2 className={styles.title}>
             KICKSTART YOUR
             <span className={styles.fancy}> BRAND </span>
---
+++ src/components/OurServices/OurServices.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './OurServices.module.css'
+type Props = {}
+
+const OurServices = (props: Props) => {
+    return (
+        <div className={styles.bgyello}>
+            Our Services
+        </div>
+    )
+}
+
+export default OurServices
