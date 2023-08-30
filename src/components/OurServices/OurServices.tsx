4a0f75440ec6adf7e8dc04f376767b18455eb4c4
Aniket <57931002+aniket-dandekar@users.noreply.github.com>
2023-08-30 17:07:02 +0530

    footer wave + our services desktop version + our services animations

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -1,43 +1,88 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './OurServices.module.css'
+import { AnimationControls, motion } from 'framer-motion'
+import { useInView } from 'framer-motion'
+
 type Props = {}
 
 const OurServices = (props: Props) => {
+
+    const ref = useRef(null)
+    const isInView = useInView(ref, { once: true, amount: 0.2 })
+
+    const fadeInAnimationVariants = {
+        initial: {
+            opacity: 0,
+            x: -500,
+        },
+        animate: (index: number) => ({
+            opacity: isInView ? 1 : 0,
+            x: isInView ? 0 : -500,
+            transition : {
+                delay : 0.4 * index,
+            }
+        })
+    }
+
+    // useEffect(() => {
+    //     console.log("Element is in view: ", isInView)
+    // }, [isInView])
+
     return (
-        <div className={styles.our_services}>
+        <div className={styles.our_services} ref={ref}>
             <h4>OUR SERVICES</h4>
 
-            <div className={styles.parent1}>
+            <motion.div className={styles.parent1}
+                variants={fadeInAnimationVariants}
+                initial='initial'
+                animate='animate'
+                custom={1}
+            >
                 <div></div>
                 <div></div>
                 <div className={styles.content}>
                     Website Designing & Development
                 </div>
-            </div>
+            </motion.div>
 
-            <div className={styles.parent2}>
+            <motion.div className={styles.parent2}
+                variants={fadeInAnimationVariants}
+                initial='initial'
+                animate='animate'
+                custom={2}
+            >
                 <div></div>
                 <div></div>
                 <div className={styles.content}>
                     Social Media Management
                 </div>
-            </div>
+            </motion.div>
 
-            <div className={styles.parent3}>
+            <motion.div className={styles.parent3}
+                variants={fadeInAnimationVariants}
+                initial='initial'
+                animate='animate'
+                custom={3}
+            >
                 <div></div>
                 <div></div>
                 <div className={styles.content}>
                     Search Engine Optimization
                 </div>
-            </div>
+            </motion.div>
 
-            <div className={styles.parent4}>
+            <motion.div className={styles.parent4}
+                variants={fadeInAnimationVariants}
+                initial='initial'
+                animate='animate'
+                custom={4}
+            >
                 <div></div>
                 <div></div>
                 <div className={styles.content}>
                     GRAPHIC DESIGNING
                 </div>
-            </div>
+            </motion.div>
         </div>
     )
 }
