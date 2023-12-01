4b311f8c381015743dfbf31a8f667283e522d411
Aniket <57931002+aniket-dandekar@users.noreply.github.com>
2023-12-01 12:38:54 +0530

    whatsapp + call btn improvements

--- src/feature/Call-button/CallIcon.tsx
+++ src/feature/Call-button/CallIcon.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FaPhone } from "react-icons/fa6";
-import styles from './CallIcon.module.css'
+import './CallIcon.css'
 import Link from 'next/link';
 
 type Props = {}
 
 function CallIcon({ }: Props) {
   return (
-    <div className={styles.whatsapp_icon}>
+    <div className='call_icon'>
       <Link aria-label="Call us directly" target="_blank" href={'tel:+919011570451'}>
         <button id="phonecall">
           <FaPhone />
--- src/feature/Whatsapp-button/WhatsappIcon.tsx
+++ src/feature/Whatsapp-button/WhatsappIcon.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BsWhatsapp } from "react-icons/bs";
-import styles from './WhatsappIcon.module.css'
+import './WhatsappIcon.css'
 import Link from 'next/link';
 
 type Props = {}
 
 function WhatsappIcon({ }: Props) {
   return (
-    <div className={styles.whatsapp_icon}>
+    <div className='whatsapp_icon'>
       <Link aria-label="Contact us on whatsapp" target="_blank" href="https://wa.me/919011570451">
         <button id="whatsapp">
           {/* <i className="fa-brands fa-whatsapp"></i> */}
